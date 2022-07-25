0637ba4a1d1cb7da0bf806209c3fd4b90d07e29d
cxkj_cjn <chenjianing@richinfo.cn>
2022-07-25 14:59:17 +0800

    初始化button

--- test/main.js
+++ test/main.js
@@ -1,12 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+// 导入组件库
+import McUI from '../packages'
 
 Vue.config.productionTip = false
 
+Vue.use(McUI)
+
 new Vue({
-  router,
-  store,
   render: h => h(App)
 }).$mount('#app')
